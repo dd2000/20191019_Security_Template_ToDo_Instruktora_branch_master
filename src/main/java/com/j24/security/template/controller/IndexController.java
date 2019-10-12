78b029d7b8f7d24b0c9ac1cd7e430e79ae935227
Paweł Recław <pawel.reclaw@gmail.com>
2019-10-12 14:56:15 +0200

    Lock and remove user

--- src/main/java/com/j24/security/template/controller/IndexController.java
+++ src/main/java/com/j24/security/template/controller/IndexController.java
@@ -1,10 +1,16 @@
 package com.j24.security.template.controller;
 
+import com.j24.security.template.model.dto.UserRegistrationRequest;
+import com.j24.security.template.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping(path = "/")
@@ -12,6 +18,9 @@ public class IndexController {
 
     @Autowired
     private String myName;
+
+    @Autowired
+    private AccountService accountService;
 
     @GetMapping("/")
     public String getIndexPage(Model model) {
@@ -23,5 +32,25 @@ public class IndexController {
     @GetMapping("/login")
     public String loginForm() {
         return "login-form";
+    }
+
+    @GetMapping("/register")
+    public String registrationForm() {
+        return "registration-form";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid UserRegistrationRequest request,
+                           BindingResult bindingResult) {
+        if (!request.arePasswordsEqual()) {
+            return "registration-form";
+        }
+        if (bindingResult.hasErrors()) {
+            return "registration-form";
+        }
+        if (!accountService.register(request)) {
+            return "registration-form";
+        }
+        return "redirect:/login";
     }
 }
--- src/main/java/com/j24/security/template/service/AccountService.java
+++ src/main/java/com/j24/security/template/service/AccountService.java
@@ -1,17 +1,29 @@
 package com.j24.security.template.service;
 
 import com.j24.security.template.model.Account;
+import com.j24.security.template.model.AccountRole;
+import com.j24.security.template.model.dto.UserRegistrationRequest;
 import com.j24.security.template.repository.AccountRepository;
+import com.j24.security.template.repository.AccountRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class AccountService {
     @Autowired
     private AccountRepository accountRepository;
+
+    @Autowired
+    private AccountRoleRepository accountRoleRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<Account> getAll() {
         return accountRepository.findAll();
@@ -37,5 +49,29 @@ public class AccountService {
 
             accountRepository.save(account);
         }
+    }
+
+    public boolean register(UserRegistrationRequest request) {
+        if (accountRepository.existsByUsername(request.getUsername())) {
+            return false;
+        }
+
+        Account account = new Account();
+        account.setUsername(request.getUsername());
+        account.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        account.setRoles(findRolesByName("USER"));
+
+        accountRepository.save(account);
+
+        return true;
+    }
+
+    public Set<AccountRole> findRolesByName(String... roles) {
+        Set<AccountRole> accountRoles = new HashSet<>();
+        for (String role : roles) {
+            accountRoleRepository.findByName(role).ifPresent(accountRoles::add);
+        }
+        return accountRoles;
     }
 }
